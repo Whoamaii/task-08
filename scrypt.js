dfd29199684bf5ae6d641db75e92ca69e32c9895
Whoamaii <110328724+Whoamaii@users.noreply.github.com>
2022-12-06 20:15:15 +0200

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,6 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, index) {
-  delete array[index];
-  return array.filter((item) => item != undefined);
+   return array.filter((item) => item != index);
 }
 console.log(removeElement(array, 4));
